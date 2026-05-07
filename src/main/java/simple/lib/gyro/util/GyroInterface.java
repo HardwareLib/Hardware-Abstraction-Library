d3d10b7626038f5e179de48a5f941376f9298be7
macsk <mac.skaanes@gmail.com>
2026-05-06 20:07:18 -0400

    working on version 0.2 now hopeful initial test release soon?

---
+++ src/main/java/simple/lib/gyro/util/GyroInterface.java
@@ -0,0 +1,11 @@
+package simple.lib.gyro.util;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+
+public interface GyroInterface {
+    public abstract Rotation2d getHeading();
+    public default Rotation2d getHeadingRate() {return Rotation2d.kZero;}
+    public abstract Rotation3d getOrientation();
+    public default Rotation3d getAngularVelocity() {return Rotation3d.kZero;}
+}
